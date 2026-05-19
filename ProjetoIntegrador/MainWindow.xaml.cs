2b5e8664960bc5c3ff98a599ea683e0cfaa6cbcb
Victor <victor005152@gmail.com>
2026-05-18 21:15:29 -0300

    Tela de Historico

--- ProjetoIntegrador/MainWindow.xaml.cs
+++ ProjetoIntegrador/MainWindow.xaml.cs
@@ -19,9 +19,12 @@ namespace ProjetoIntegrador
         public MainWindow()
         {
             InitializeComponent();
+
+            List<HistVendas> historico = new List<HistVendas>();
+            historico.Add(new HistVendas() { Data = DateTime.Now.Date, Descricao = "Camiseta", Valor = 100.0, Qtd = 2, Cliente = "João Silva", CPF = 123456789, Codigo = 5001 });
+            Historico1.ItemsSource = historico;
         }
 
-<<<<<<< HEAD
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -31,7 +34,34 @@ namespace ProjetoIntegrador
         {
 
         }
-=======
->>>>>>> bb2f19a1cdaa2fa63d8c64ab17335c7e7f3b7406
+
+        private void Historico1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HistVendas LinhaSelecionada = Historico1.SelectedItem as HistVendas;
+            if (LinhaSelecionada != null)
+            {
+                MessageBox.Show($"NOTA FISCAL\n\n" + $"Data: {LinhaSelecionada.Data.ToShortDateString()}\n" + $"----------------------------------\n" + $"Produto: {LinhaSelecionada.Descricao}\n" + $"Quantidade: {LinhaSelecionada.Qtd}\n" + $"Valor Unitário: R$ {LinhaSelecionada.Valor}\n" + $"----------------------------------\n" + $"Cliente: {LinhaSelecionada.Cliente}\n" + $"CPF: {LinhaSelecionada.CPF}\n" + $"Código: {LinhaSelecionada.Codigo}",
+     $"Cliente: {LinhaSelecionada.Cliente}\n" + "Sistema de Vendas", MessageBoxButton.OK, MessageBoxImage.Information);         
+
+            }
+        }
     }
+
+}
+public class HistVendas
+{
+    public DateTime Data { get; set; }
+    public string Descricao { get; set; }
+    public double Valor { get; set; }
+    public int Qtd { get; set; }
+    public string Cliente { get; set; }
+    public int CPF { get; set; }
+    public int Codigo { get; set; }
+
+
 }
