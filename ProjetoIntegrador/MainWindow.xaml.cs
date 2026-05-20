633a2ec8d6178624f524a719b472358e26425e01
Victor <victor005152@gmail.com>
2026-05-19 21:19:11 -0300

    Atualização 19/05/2026

--- ProjetoIntegrador/MainWindow.xaml.cs
+++ ProjetoIntegrador/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace ProjetoIntegrador
     {
         public MainWindow()
         {
+            List<Estoque> ProdutoList = new List<Estoque>();
+            ProdutoList.Add(new Estoque() { nome = "camisa", qnt = 1, codbarras = 123456789 });
+            DGestoque.ItemsSource = ProdutoList;
             InitializeComponent();
 
             List<HistVendas> historico = new List<HistVendas>();
@@ -46,22 +49,56 @@ namespace ProjetoIntegrador
             if (LinhaSelecionada != null)
             {
                 MessageBox.Show($"NOTA FISCAL\n\n" + $"Data: {LinhaSelecionada.Data.ToShortDateString()}\n" + $"----------------------------------\n" + $"Produto: {LinhaSelecionada.Descricao}\n" + $"Quantidade: {LinhaSelecionada.Qtd}\n" + $"Valor Unitário: R$ {LinhaSelecionada.Valor}\n" + $"----------------------------------\n" + $"Cliente: {LinhaSelecionada.Cliente}\n" + $"CPF: {LinhaSelecionada.CPF}\n" + $"Código: {LinhaSelecionada.Codigo}",
-     $"Cliente: {LinhaSelecionada.Cliente}\n" + "Sistema de Vendas", MessageBoxButton.OK, MessageBoxImage.Information);         
+     $"Cliente: {LinhaSelecionada.Cliente}\n" + "Sistema de Vendas", MessageBoxButton.OK, MessageBoxImage.Information);
 
             }
         }
+        public class HistVendas
+        {
+            public DateTime Data { get; set; }
+            public string Descricao { get; set; }
+            public double Valor { get; set; }
+            public int Qtd { get; set; }
+            public string Cliente { get; set; }
+            public int CPF { get; set; }
+            public int Codigo { get; set; }
+        }
+        public class Estoque
+        {
+            public string nome { get; set; }
+            public int qnt { get; set; }
+            public int codbarras { get; set; }
+
+            private void Cad_Click(object sender, RoutedEventArgs e)
+            {
+                Estoque estoqueSelecionado = DGestoque.SelectedItem as Estoque;
+
+                if (estoqueSelecionado != null)
+                {
+                    MessageBox.Show($"{estoqueSelecionado.nome}, {estoqueSelecionado.qnt}, {estoqueSelecionado.codbarras}");
+
+                }
+                else
+                {
+                    MessageBox.Show("Selecioen uma item");
+                }
+            }
+
+            private void Exc_Click(object sender, RoutedEventArgs e)
+            {
+                Estoque estoqueSelecionado = DGestoque.SelectedItem as Estoque;
+
+                if (estoqueSelecionado != null)
+                {
+                    ProdutoList.Remove(estoqueSelecionado);
+                    DGestoque.Items.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Selecioen uma item");
+                }
+            }
+
+        }
     }
-
-}
-public class HistVendas
-{
-    public DateTime Data { get; set; }
-    public string Descricao { get; set; }
-    public double Valor { get; set; }
-    public int Qtd { get; set; }
-    public string Cliente { get; set; }
-    public int CPF { get; set; }
-    public int Codigo { get; set; }
-
-
 }
